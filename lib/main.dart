5a8be83ab46d70886e380318286feb4acfc4be6a
Sergio Monteros <sergio.monteros@twiinshrm.com>
2024-12-11 20:35:08 -0500

    avance en diseño

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,32 @@
 import 'package:devtimer/views/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() {
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Configuración para Android
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  // Configuración para iOS
+  const DarwinInitializationSettings initializationSettingsDarwin =
+      DarwinInitializationSettings(
+    requestAlertPermission: true,
+    requestBadgePermission: true,
+    requestSoundPermission: true,
+  );
+
+  // Configuración general
+  const InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsDarwin,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
   runApp(const MyApp());
 }
 
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,9 +1,14 @@
 import 'dart:async';
+import 'dart:typed_data';
 import 'dart:ui';
 
+import 'package:devtimer/main.dart';
 import 'package:devtimer/views/dialogs-views/settings-pomodoro.dart';
 
 import 'package:flutter/material.dart';
+
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
@@ -17,12 +22,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int _seconds = 25 * 60; // 25 minutos en segundos
   late Timer _timer;
-  int _counter = 0;
-
-  @override
-  void initState() {
-    super.initState();
-  }
+  bool _isTimerRunning = false;
+  int currentPageIndex = 0;
+  NavigationDestinationLabelBehavior labelBehavior =
+      NavigationDestinationLabelBehavior.onlyShowSelected;
 
   generateDialog(dynamic classDialog, double heightCard) {
     return showDialog(
@@ -72,16 +75,134 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  @override
+  void initState() {
+    super.initState();
+    _checkNotificationPermission();
+  }
+
+  Future<void> _checkNotificationPermission() async {
+    var status = await Permission.notification.status;
+
+    if (status.isDenied || status.isPermanentlyDenied) {
+      // Solicitar permiso si es necesario
+      final result = await Permission.notification.request();
+      if (result.isGranted) {
+        print("Permiso de notificaciones concedido");
+      } else {
+        print("Permiso de notificaciones denegado");
+      }
+    } else if (status.isGranted) {
+      print("Permiso ya concedido");
+    }
+  }
+
+  Future<void> _showNotification(seconds) async {
+    const AndroidNotificationDetails androidDetails =
+        AndroidNotificationDetails(
+      'timer_channel_id',
+      'Timer Notifications',
+      channelDescription: 'Shows the countdown timer',
+      importance: Importance.low,
+      priority: Priority.low,
+      ongoing: true,
+      vibrationPattern: null,
+      enableVibration: false,
+    );
+
+    const NotificationDetails notificationDetails =
+        NotificationDetails(android: androidDetails);
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'Focus Time',
+      'Time remaining: $seconds seconds',
+      notificationDetails,
+    );
+  }
+
+  Future<void> _showNotificationComplete() async {
+    const AndroidNotificationDetails androidDetails =
+        AndroidNotificationDetails(
+      'timer_channel_id',
+      'Timer Notifications',
+      channelDescription: 'Shows the countdown timer',
+      importance: Importance.max,
+      priority: Priority.high,
+      ongoing: false,
+    );
+
+    const NotificationDetails notificationDetails =
+        NotificationDetails(android: androidDetails);
+
+    await flutterLocalNotificationsPlugin.show(
+      1,
+      'Timer Finished',
+      'The timer has completed!',
+      notificationDetails,
+    );
+  }
+
+  void _startOrStopTimer() {
+    if (_isTimerRunning) {
+      _stopTimer(); // Detener el temporizador si está en marcha
+    } else {
+      _startTimer(); // Empezar el temporizador si está detenido
+    }
+  }
+
+  void _stopTimer() {
+    _timer?.cancel(); // Detener el temporizador
+    setState(() {
+      _seconds = 25 * 60;
+      _isTimerRunning = false; // Cambiar el estado a detenido
+    });
+    _updateNotification(0);
+  }
+
   void _startTimer() {
+    setState(() {
+      _isTimerRunning = true; // El temporizador ha comenzado
+    });
+
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       if (_seconds > 0) {
         setState(() {
           _seconds--;
         });
+        _updateNotification(_formattedTime);
       } else {
-        _timer.cancel(); // Detener el temporizador cuando llegue a 0
+        _timer?.cancel(); // Detener el temporizador cuando llegue a 0
+        _showNotificationComplete();
+        setState(() {
+          _isTimerRunning = false; // El temporizador ha terminado
+        });
       }
     });
+  }
+
+  // Actualizar la notificación con el tiempo restante
+  Future<void> _updateNotification(seconds) async {
+    AndroidNotificationDetails androidDetails = AndroidNotificationDetails(
+      'timer_channel_id',
+      'Timer Notifications',
+      channelDescription: 'Shows the countdown timer',
+      importance: Importance.low,
+      priority: Priority.low,
+      ongoing: true, // Mantener la notificación en segundo plano
+      vibrationPattern: Int64List.fromList([]), // Sin vibración
+      enableVibration: false, // Desactiva la vibración
+    );
+
+    NotificationDetails notificationDetails =
+        NotificationDetails(android: androidDetails);
+
+    await flutterLocalNotificationsPlugin.show(
+      0, // ID de la notificación (se mantiene el mismo ID)
+      'Timer Countdown',
+      'Time remaining: $seconds seconds', // Mostrar el tiempo restante
+      notificationDetails,
+    );
   }
 
   @override
@@ -97,11 +218,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   String _twoDigits(int n) {
-    if (n >= 10) {
-      return '$n';
-    } else {
-      return '0$n';
-    }
+    return n >= 10 ? '$n' : '0$n';
   }
 
   @override
@@ -111,19 +228,14 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Container(
           margin: const EdgeInsets.only(left: 5),
-          child: Text(
-            widget.title,
-          ),
+          child: Text(widget.title),
         ),
       ),
       drawer: Drawer(
         child: ListView(
           children: [
-            Icon(
-              Icons.coffee,
-              size: 90,
-              color: Color.fromARGB(255, 153, 105, 43),
-            ),
+            Icon(Icons.coffee,
+                size: 90, color: Color.fromARGB(255, 153, 105, 43)),
             ListTile(
               leading: Icon(Icons.access_alarms_rounded),
               title: Text('Adjust pomodoro'),
@@ -151,28 +263,117 @@ class _HomePageState extends State<HomePage> {
               Text(
                 _formattedTime,
                 style: const TextStyle(
-                    fontFamily: "Doto",
-                    color: Colors.white,
-                    fontSize: 80,
-                    fontWeight: FontWeight.w500),
+                  fontFamily: "Tiny5",
+                  color: Colors.white,
+                  fontSize: 80,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
               Image.asset(
                 'assets/gifs/CoffeCompleteWhite.gif',
                 height: 425.0,
                 width: 425.0,
               ),
-              const SizedBox(
-                height: 15,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  _startTimer();
-                },
-                child: const Text('Star Day'),
+              const SizedBox(height: 15),
+              Row(
+                children: [
+                  const SizedBox(
+                    width: 30,
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: const CircleBorder(),
+                        padding: const EdgeInsets.all(
+                            15), // Ensure padding for circular shape
+                      ),
+                      onPressed:
+                          _startOrStopTimer, // Llamar al método que empieza o detiene el temporizador
+                      child: Icon(
+                          _isTimerRunning ? Icons.pause : Icons.play_arrow),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 30,
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: const CircleBorder(),
+                        padding: const EdgeInsets.all(
+                            15), // Ensure padding for circular shape
+                      ),
+                      onPressed:
+                          _startOrStopTimer, // Llamar al método que empieza o detiene el temporizador
+                      child: Icon(Icons.stop),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: const CircleBorder(),
+                        padding: const EdgeInsets.all(
+                            15), // Ensure padding for circular shape
+                      ),
+                      onPressed:
+                          _startOrStopTimer, // Llamar al método que empieza o detiene el temporizador
+                      child: Icon(Icons.volume_off),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                ],
               ),
             ],
           ),
         ),
+      ),
+      bottomNavigationBar: NavigationBar(
+        labelBehavior: labelBehavior,
+        selectedIndex: currentPageIndex,
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Color.fromARGB(255, 240, 185, 113),
+        backgroundColor: const Color.fromARGB(255, 153, 105, 43),
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(
+              Icons.coffee,
+              color: Color.fromARGB(255, 153, 105, 43),
+            ),
+            icon: Icon(Icons.coffee, color: Color.fromARGB(255, 240, 185, 113)),
+            label: 'Pomodoro',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(
+              Icons.update_outlined,
+              color: const Color.fromARGB(255, 153, 105, 43),
+            ),
+            icon: Icon(
+              Icons.update_outlined,
+              color: Color.fromARGB(255, 240, 185, 113),
+            ),
+            label: 'Short Break',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(
+              Icons.self_improvement_sharp,
+              color: const Color.fromARGB(255, 153, 105, 43),
+            ),
+            icon: Icon(
+              Icons.self_improvement_sharp,
+              color: Color.fromARGB(255, 240, 185, 113),
+            ),
+            label: 'Long Break',
+          ),
+        ],
       ),
     );
   }

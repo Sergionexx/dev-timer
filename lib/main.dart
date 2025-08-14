92be9cd70e26aaebaf95fbdda011b1598f367a74
Sergionexx <sergionexx1@gmail.com>
2025-08-14 11:51:11 -0500

    Refactor build configuration and remove unused dependencies; implement signing configuration for release builds

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,14 @@ import 'package:devtimer/views/recovery_password_screen.dart';
 import 'package:devtimer/views/register_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
+//import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  MobileAds.instance.initialize();
+  //MobileAds.instance.initialize();
   // Configuración para Android
   const AndroidInitializationSettings initializationSettingsAndroid =
       AndroidInitializationSettings('@mipmap/ic_launcher');
--- lib/widgets/ads/ad-banner.dart
+++ lib/widgets/ads/ad-banner.dart
@@ -1,55 +1,55 @@
-import 'package:flutter/material.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
+// import 'package:flutter/material.dart';
+// import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-class AdBanner extends StatefulWidget {
-  @override
-  _AdBannerState createState() => _AdBannerState();
-}
+// class AdBanner extends StatefulWidget {
+//   @override
+//   _AdBannerState createState() => _AdBannerState();
+// }
 
-class _AdBannerState extends State<AdBanner> {
-  late BannerAd _bannerAd;
-  bool _isAdLoaded = false;
+// class _AdBannerState extends State<AdBanner> {
+//   late BannerAd _bannerAd;
+//   bool _isAdLoaded = false;
 
-  @override
-  void initState() {
-    super.initState();
-    _createBannerAd();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     _createBannerAd();
+//   }
 
-  void _createBannerAd() {
-    _bannerAd = BannerAd(
-      adUnitId: 'ca-app-pub-3940256099942544/6300978111', // ca-app-pub-7890676436731935/1897870174
-      size: AdSize.banner,
-      request: AdRequest(),
-      listener: BannerAdListener(
-        onAdLoaded: (_) {
-          setState(() {
-            _isAdLoaded = true;
-          });
-        },
-        onAdFailedToLoad: (ad, error) {
-          ad.dispose();
-          print('Error al cargar el anuncio: $error');
-        },
-      ),
-    )..load();
-  }
+//   void _createBannerAd() {
+//     _bannerAd = BannerAd(
+//       adUnitId: 'ca-app-pub-3940256099942544/6300978111', // ca-app-pub-7890676436731935/1897870174
+//       size: AdSize.banner,
+//       request: AdRequest(),
+//       listener: BannerAdListener(
+//         onAdLoaded: (_) {
+//           setState(() {
+//             _isAdLoaded = true;
+//           });
+//         },
+//         onAdFailedToLoad: (ad, error) {
+//           ad.dispose();
+//           print('Error al cargar el anuncio: $error');
+//         },
+//       ),
+//     )..load();
+//   }
 
-  @override
-  void dispose() {
-    _bannerAd.dispose();
-    super.dispose();
-  }
+//   @override
+//   void dispose() {
+//     _bannerAd.dispose();
+//     super.dispose();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return _isAdLoaded
-        ? Container(
-            alignment: Alignment.center,
-            width: _bannerAd.size.width.toDouble(),
-            height: _bannerAd.size.height.toDouble(),
-            child: AdWidget(ad: _bannerAd),
-          )
-        : SizedBox.shrink();
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return _isAdLoaded
+//         ? Container(
+//             alignment: Alignment.center,
+//             width: _bannerAd.size.width.toDouble(),
+//             height: _bannerAd.size.height.toDouble(),
+//             child: AdWidget(ad: _bannerAd),
+//           )
+//         : SizedBox.shrink();
+//   }
+// }
